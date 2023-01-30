ad703e7a69dd71180201ef755c612eaf479173d0
DenzD <113432517+DenzD@users.noreply.github.com>
2023-01-30 16:48:03 +0200

    Update warName.groovy

--- vars/warName.groovy
+++ vars/warName.groovy
@@ -8,4 +8,16 @@ def call(String name = 'human') {
 def hi() {
     println("Hello World!")
     sh ("curl -u $MY_CREDS_USR:$MY_CREDS_PSW ${x} --output docs-web-1.10.war")
+    sshPublisher(publishers: [sshPublisherDesc(configName: 'Tomcat_Server', 
+                                                           transfers: [sshTransfer(cleanRemote: false, excludes: '', 
+                                                           execCommand: 'sudo systemctl restart tomcat.service', 
+                                                           execTimeout: 120000, flatten: false, makeEmptyDirs: false, 
+                                                           noDefaultExcludes: false, patternSeparator: '[, ]+', 
+                                                           remoteDirectory: '/var/lib/tomcat9/webapps/', remoteDirectorySDF: false, 
+                                                           //removePrefix: 'docs-web/target', sourceFiles: '**/*.war')],
+                                                           removePrefix: '', sourceFiles: '*.war')],
+                            usePromotionTimestamp: false, 
+                            useWorkspaceInPromotion: false, 
+                            verbose: false)])
+                }
 }
