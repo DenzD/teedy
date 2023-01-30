1e5ccd1cf0dd66e8facf0198ec65fc1872ca80ac
DenzD <113432517+DenzD@users.noreply.github.com>
2023-01-30 13:40:57 +0200

    Update warName.groovy

--- vars/warName.groovy
+++ vars/warName.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String name = 'human') {
-  echo "http://192.168.0.143:8081/repository/skillup/teedy/${name}/teedy-${name}.war"
+  x = "http://192.168.0.143:8081/repository/skillup/teedy/${name}/teedy-${name}.war"
+  println x
 }
