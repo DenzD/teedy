11c60a7d239dab1b88949155ccd97b6d4b9d1d22
DenzD <113432517+DenzD@users.noreply.github.com>
2023-01-31 12:15:12 +0200

    Update warName.groovy

--- vars/warName.groovy
+++ vars/warName.groovy
@@ -5,7 +5,7 @@
 def call(String name = 'human') {
   x = "http://192.168.0.143:8081/repository/skillup/teedy/${name}/teedy-${name}.war"
   println x
-  String a = ${name}
+  String a = "${name}"
   String[] str;       
   str = a.split('a');              
   for( String values : str )       
